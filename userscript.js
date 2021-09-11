d6529a2a5dd5d2bb49678306ff8b0e4d32f9bc27
saglikguvencem <88438702+saglikguvencem@users.noreply.github.com>
2021-09-12 00:12:46 +0300

    Add files via upload

---
+++ userscript.js
@@ -0,0 +1,12 @@
+function tansiyon(){
+    window.location="tansiyon.html";
+}
+function covid(){
+    window.location="covidhaber.html";
+}
+function alt(){
+    window.location="https://saglikguvencemdestek.tawk.help/article/alt-testi-nas%C4%B1l-olunur";
+}
+function insulin(){
+    window.location="https://saglikguvencemdestek.tawk.help/article/i%CC%87ns%C3%BClin-testi-nas%C4%B1l-olunur";
+}
